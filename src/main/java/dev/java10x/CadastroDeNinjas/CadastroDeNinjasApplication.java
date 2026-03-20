95a5965f24fb4e147be2d26c6faf786d44a596d9
Yuri Sanchez <yuri.tecnologia@outlook.com>
2026-03-20 18:29:13 -0300

    Criado a controller

--- src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -3,11 +3,13 @@ package dev.java10x.CadastroDeNinjas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+		SpringApplication.run(CadastroDeNinjasApplication.class, Arrays.toString(args));
 	}
 
 }
