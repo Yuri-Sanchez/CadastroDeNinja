8e140484ac18e8711ee2ee088c0797ac88b618c9
Yuri Sanchez <yuri.tecnologia@outlook.com>
2026-04-08 15:33:42 -0300

    chore: Inclui a versao do DTO pela entidade e mapeia manualmente no MissoesMapper os caminhos entidade x dto

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesMapper.java
@@ -1,4 +1,31 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Component;
+
+@Component
+// @Mapper - Jeito mais fácil de fazer, deixa tudo automático
+
 public class MissoesMapper {
+
+    // Transforma uma entidade em DTO
+    public MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+
+        return missoesModel;
+    }
+
+    // Transforma um DTO em uma entidade
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+
+        return missoesDTO;
+    }
 }
